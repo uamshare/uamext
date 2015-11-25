13b45bcc2f06564fdeef9e6139056e855b0e0467
uam <uamshare@gmail.com>
2015-11-25 17:29:28 +0700

    Restruture File

--- widgets/form/Select.php
+++ widgets/form/Select.php
@@ -15,7 +15,7 @@ class Select extends \yii\widgets\InputWidget
 	public function init()
     {
         parent::init();
-		$this->options['id'] = 'uam-select-' . $this->attribute;
+		//$this->options['id'] = $this->getId() . $this->attribute;//'uam-select-' . $this->attribute;
 		$this->options['class'] = 'form-control';
 		if($this->data == null){
 			throw new InvalidConfigException('The "data" property must be set.');
@@ -29,10 +29,14 @@ class Select extends \yii\widgets\InputWidget
 		$id = $this->options['id'];
 		//Register DtGridViewAsset
 		SelectAsset::register($view);
-		
-		$content = Html::activeDropDownList($this->model, $this->attribute, $this->data, $this->options);
+		if($this->model){	
+			$content = Html::activeDropDownList($this->model, $this->attribute, $this->data, $this->options);
+		}else{
+			$content = Html::dropDownList($this->options['id'], $this->attribute, $this->data, $this->options);
+			
+		}
 		$clientOptions = Json::encode($this->clientOptions);
-		$view->registerJs("jQuery('#$id').select2($clientOptions);");
+		$view->registerJs("var uam_select".$this->getId()."=jQuery('#$id').select2($clientOptions);");
 		
         return $content;
     }
